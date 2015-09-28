873e8f86a092e4488ab1d27feff45f12bcfc3d4b
rachael berecka & lindsay culver <student@epicodus.com>
2015-09-28 10:52:04 -0700

    Add category component to display categories on index

---
+++ app/models/listing.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr(),
+  seller: DS.attr(),
+  price: DS.attr(),
+  description: DS.attr(),
+  image: DS.attr(),
+  category: DS.belongsTo('category', { async: true})
+});
