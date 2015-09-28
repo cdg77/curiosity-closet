40dba04580ff137f33a1373c3074b152ab328b72
rachael berecka & lindsay culver <student@epicodus.com>
2015-09-28 11:49:14 -0700

    Add listing to category

--- app/components/new-listing.js
+++ app/components/new-listing.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewListing: false,
+  actions: {
+    listingFormShow() {
+      this.set('addNewListing', true);
+    },
+    saveListing() {
+      var params = {
+        title: this.get('title'),
+        seller: this.get('seller'),
+        price: this.get('price'),
+        description: this.get('description'),
+        image: this.get('image'),
+        category: this.get('category')
+      };
+      this.set('addNewListing', false);
+      this.sendAction('saveListing', params);
+    }
+  }
 });
